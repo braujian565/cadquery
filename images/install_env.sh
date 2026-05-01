0e6abc57598fa88b1ec3caffe8b218e687dcf72c
adam-urbanczyk <13981538+adam-urbanczyk@users.noreply.github.com>
2026-05-01 09:47:51 +0200

    Do not install in editable mode

--- images/install_env.sh
+++ images/install_env.sh
@@ -2,6 +2,6 @@ head -n -1 $MYDIR/environment.yml  > $MYDIR/environment_.yml
 micromamba install -y -q -n base git
 micromamba install -y -q -n base -f $MYDIR/environment_.yml
 micromamba install -y -q -n base jupyterlab
-micromamba run pip install -e $MYDIR/cq
+micromamba run pip install $MYDIR/cq
 micromamba list
 micromamba clean --all --yes
